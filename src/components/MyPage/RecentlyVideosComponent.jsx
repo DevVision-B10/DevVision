5f9caee4d58ead84156005238ae3bc0a6d8cb8a1
muyeon <gmakin36@gmail.com>
2024-06-19 02:07:10 +0900

    implemented recently viewed videos feature - 최근 본 영상 기능 구현 완료 - TODO: 최근 본 영상 페이지 UI 깨지는 현상 수정

--- src/components/MyPage/RecentlyVideosComponent.jsx
+++ src/components/MyPage/RecentlyVideosComponent.jsx
@@ -1,3 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios'; // axios import 추가
+import { useEffect, useState } from 'react';
+import supabase from '../../supabase/config';
 import {
   RecentlyVideosCard,
   RecentlyVideosCardList,
@@ -6,7 +10,72 @@ import {
   RecentlyVideosWrapper
 } from './RecentlyVideosComponentStyle';
 
-function RecentlyVideosComponent() {
+function RecentlyVideosComponent({ user }) {
+  const [recentVideos, setRecentVideos] = useState([]);
+  const [videoData, setVideoData] = useState([]);
+
+  useEffect(() => {
+    const getRecentVideoData = async () => {
+      if (user && user.email) {
+        const { data: findUser, error } = await supabase
+          .from('Users')
+          .select('recentVideo')
+          .eq('email', user.email)
+          .single();
+        if (error) {
+          console.error(error);
+          return;
+        }
+        const recentData = JSON.parse(findUser.recentVideo || '[]');
+        setRecentVideos(recentData);
+      }
+    };
+
+    if (user && user.email) {
+      getRecentVideoData();
+    }
+  }, [user]);
+
+  const {
+    data: videoDataResponse,
+    isLoading,
+    isError
+  } = useQuery({
+    queryKey: ['recentVideos'],
+    queryFn: async () => {
+      if (recentVideos.length === 0) {
+        return [];
+      }
+
+      const videoIds = recentVideos.join(',');
+
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_YOUTUBE_SEARCH_VIDEO_URL}`, {
+          params: {
+            part: 'snippet',
+            id: videoIds,
+            key: import.meta.env.VITE_YOUTUBE_KEY
+          }
+        });
+        return response.data.items;
+      } catch (error) {
+        throw new Error('Failed to fetch video data');
+      }
+    },
+    enabled: recentVideos.length > 0
+  });
+
+  useEffect(() => {
+    if (videoDataResponse) {
+      setVideoData(videoDataResponse);
+    }
+  }, [videoDataResponse]);
+
+  if (isLoading) return <div>loading</div>;
+  if (isError) return <div>Error: {isError.message}</div>;
+
+  console.log(videoData);
+
   return (
     <RecentlyVideosWrapper>
       <RecentlyVideosSection>
@@ -14,18 +83,12 @@ function RecentlyVideosComponent() {
           <h1>최근 본 영상</h1>
         </RecentlyVideosTitleWrapper>
         <RecentlyVideosCardList>
-          <RecentlyVideosCard>
-            <img src="/src/assets/pikachu.png" />
-            <p>제목</p>
-          </RecentlyVideosCard>
-          <RecentlyVideosCard>
-            <img src="/src/assets/pikachu.png" />
-            <p>제목</p>
-          </RecentlyVideosCard>
-          <RecentlyVideosCard>
-            <img src="/src/assets/pikachu.png" />
-            <p>제목</p>
-          </RecentlyVideosCard>
+          {videoData.map((video, index) => (
+            <RecentlyVideosCard key={index}>
+              <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+              <p>{video.snippet.title}</p>
+            </RecentlyVideosCard>
+          ))}
         </RecentlyVideosCardList>
       </RecentlyVideosSection>
     </RecentlyVideosWrapper>
