4d02b2d8c2061b0de63a7c175d0f98e43b560baa
muyeon <gmakin36@gmail.com>
2024-06-19 00:30:21 +0900

    update: clicking on a video adds a feature to the video ID database - 동영상 클릭시 영상 ID 데이터베이스에 추가되는 기능 추가

--- src/components/LogIn/LogInRedirect.jsx
+++ src/components/LogIn/LogInRedirect.jsx
@@ -25,7 +25,7 @@ function LogInRedirect() {
 
     const checkUserExists = async (userId) => {
       const { data, error } = await supabase.from('Users').select('*').eq('userId', userId).single();
-      if (error) throw new Error('사용자 조회 실패');
+      if (error) return 0;
       return data;
     };
 
@@ -42,7 +42,9 @@ function LogInRedirect() {
     const handleAuthCallback = async () => {
       try {
         const user = await getSession();
+
         const userData = getUserData(user);
+
         const existingUser = await checkUserExists(userData.userId);
 
         if (!existingUser) await insertUser(userData);
--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,30 +1,56 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import dayjs from 'dayjs';
+import supabase from '../../supabase/config';
 import useRecentVideoStore from '../../zustand/recent-video';
+import useLogStore from '../../zustand/user-log';
 
 /**
  * 1. 최근 본 영상에 대한 전역 관리를 추가한다.
  * 2. 배열 형식으로 videoId 를 넣는다.
  * 3. 사용자가 클릭했을때 전역 상태 값을 업데이트 한다.
  * 4. string 으로 변경해 해당 유저에 컬럼을 update 한다.
- *
  */
 
 function TestPage() {
   const recent = useRecentVideoStore((state) => state.recent);
   const addRecentVideo = useRecentVideoStore((state) => state.addRecentVideo);
+  const user = useLogStore((state) => state.user);
 
   const { data: test, isLoading } = useQuery({
     queryKey: ['test'],
     queryFn: () => axios.get(import.meta.env.VITE_TEST_URL)
   });
 
-  const handleOnClickCard = (videoId) => {
-    addRecentVideo(videoId);
-    console.log(videoId);
-  };
-  console.log(test);
+  const handleOnClickCard = async (videoId) => {
+    await addRecentVideo(videoId);
 
+    const updateRecent = useRecentVideoStore.getState().recent;
+    console.log(updateRecent);
+
+    if (user) {
+      const { email } = user;
+      const recentString = JSON.stringify(updateRecent);
+      const date = dayjs().format('YYYY-MM-DD');
+      const updateUserData = {
+        email,
+        recentVideo: recentString,
+        social: user.app_metadata.provider,
+        createdAt: date,
+        updatedAt: date
+      };
+
+      const { error } = await supabase.from('Users').upsert(updateUserData, { onConflict: ['email'] });
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('완료');
+      }
+    }
+  };
+
+  console.log(user);
   console.log(recent);
 
   if (isLoading) return 'loading';
